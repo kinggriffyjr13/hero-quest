ff14dce4a7b7c53aea804af4d9dc42c7085e19d2
kinggriffyjr13 <139810564+kinggriffyjr13@users.noreply.github.com>
2023-07-19 23:57:25 +0000

    hero's quest

--- app/game-manager.js
+++ app/game-manager.js
@@ -2,7 +2,7 @@ const player = {
   currentHealth: 10,
   attackPower: 1,
   gold: 0,
-  potions: 0
+  potions: 0,
 }
 
 function healPlayer() {
@@ -11,26 +11,33 @@ function healPlayer() {
 
 function attackEnemy() {
   // STUB
+  (currentEnemy.health -= player.attackPower)
+  drawPlayer()
+  drawCurrentEnemy()
+  healPlayer(10)  ('you have no potions')
 }
 
 function drawPlayer() {
   document.getElementById('hero-health').innerText = '💖 ' + player.currentHealth
   // TODO finish the rest of the player attributes 
-  document.getElementById('hero-gold').innerText = '🪙 '
-  document.getElementById('hero-potions').innerText = '🧪 '
-  document.getElementById('hero-attack').innerText = '⚔️ '
+  document.getElementById('hero-gold').innerText = '🪙 ' + player.gold
+  document.getElementById('hero-potions').innerText = '🧪 ' + player.potions
+  document.getElementById('hero-attack').innerText = '⚔️ ' + player.attackPower
 }
 
 function drawCurrentEnemy() {
   //  STUB
+  document.getElementById('enemy-health').innerText = '💖 ' + currentEnemy.health 
 }
 
 function enemyTurn() {
   // STUB
+
 }
 
 function rewardPlayer() {
-  // STUB
+  //
+pickRandomEnemyFromCurrentStage()
 }
 
 function buyPotion() {
